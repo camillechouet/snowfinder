6324abc8b96cd0ac05c260e43f7ce3c7eda6196e
camillechouet <camillechouet@gmail.com>
2026-05-18 13:29:33 +0200

    Add files via upload

---
+++ OneSignalSDKWorker.js
@@ -0,0 +1,3 @@
+// SnowFinder — OneSignal Service Worker
+// Ce fichier est requis pour recevoir les notifications push OneSignal
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
